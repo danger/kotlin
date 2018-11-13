1a32439ae9ba7c081184de1dc5759c0ddf275c68
Franco Meloni <franco.meloni91@gmail.com>
2018-11-13 19:30:56 +0000

    Added documentation for the Git classes

--- src/main/kotlin/com/danger/dangerkt/lib/Git.kt
+++ src/main/kotlin/com/danger/dangerkt/lib/Git.kt
@@ -4,6 +4,13 @@ import com.google.gson.annotations.SerializedName
 
 typealias FilePath = String
 
+/**
+ * The git specific metadata for a pull request
+ *
+ * @property modifiedFiles Modified file paths relative to the git root.
+ * @property createdFiles Newly created file paths relative to the git root.
+ * @property deletedFiles Removed file paths relative to the git root.
+ */
 data class Git(
     @SerializedName("modified_files") val modifiedFiles: Array<FilePath>,
     @SerializedName("created_files") val createdFiles: Array<FilePath>,
@@ -30,6 +37,16 @@ data class Git(
     }
 }
 
+/**
+ * A platform agnostic reference to a git commit.
+ *
+ * @property sha The SHA for the commit.
+ * @property author Who wrote the commit.
+ * @property committer Who shipped the code.
+ * @property message The message for the commit.
+ * @property parents SHAs for the commit's parents.
+ * @property url The URL for the commit.
+ */
 data class GitCommit(
     val sha: String?,
     val author: GitCommitAuthor,
@@ -68,6 +85,13 @@ data class GitCommit(
     }
 }
 
+/**
+ * A platform agnostic reference to a git commit.
+ *
+ * @property name The display name for the author.
+ * @property email The email for the author.
+ * @property date The ISO8601 date string for the commit.
+ */
 data class GitCommitAuthor(
         val name: String,
         val email: String,
