d3ea1efd3092bc65e9634c10ae1730a8d35f3da3
Franco Meloni <franco.meloni91@gmail.com>
2018-11-20 20:15:03 +0000

    Added documentation for GitHub classes

--- danger-kotlin-library/src/main/kotlin/com/danger/dangerkotlin/GitHub.kt
+++ danger-kotlin-library/src/main/kotlin/com/danger/dangerkotlin/GitHub.kt
@@ -3,6 +3,9 @@ package com.danger.dangerkotlin
 import com.google.gson.annotations.SerializedName
 import java.util.*
 
+/**
+ * The GitHub metadata for your pull request.
+ */
 data class GitHub(
     val issue: GitHubIssue,
     @SerializedName("pr") val pullRequest: GitHubPR,
@@ -46,6 +49,32 @@ enum class GitHubPullRequestState(val value: String) {
     LOCKED("locked")
 }
 
+/**
+ * The GitHub pull request
+ *
+ * @property number The number of the pull request.
+ * @property title The title of the pull request.
+ * @property body The markdown body message of the pull request.
+ * @property user The user who submitted the pull request.
+ * @property assignee The user who submitted the pull request.
+ * @property assignees The users who are assigned to the pull request.
+ * @property createdAt The date for when the pull request was created.
+ * @property updatedAt The date for when the pull request was updated.
+ * @property closedAt The date for when the pull request was closed.
+ * @property mergedAt The date for when the pull request was merged.
+ * @property head The merge reference for the _other_ repo.
+ * @property base The merge reference for the _this_ repo.
+ * @property state The state for the pull request: open, closed, locked, merged.
+ * @property isLocked A boolean indicating if the pull request has been locked to contributors only.
+ * @property isMerged A boolean indicating if the pull request has been merged.
+ * @property commitCount The number of commits in the pull request.
+ * @property commentCount The number of comments in the pull request.
+ * @property reviewCommentCount The number of review-specific comments in the pull request.
+ * @property additions The number of added lines in the pull request.
+ * @property deletions The number of deleted lines in the pull request.
+ * @property changedFiles The number of files changed in the pull request.
+ * @property milestone The milestone of the pull request
+ */
 data class GitHubPR(
         val number: Int,
         val title: String,
@@ -129,11 +158,23 @@ data class GitHubPR(
     }
 }
 
+/**
+ * A GitHub team
+ *
+ * @property id The UUID for the team.
+ * @property name The team name.
+ */
 data class GitHubTeam(
     val id: Int,
     val name: String
 )
 
+/**
+ * Represents the payload for a PR's requested reviewers value.
+ *
+ * @property users The list of users of whom a review has been requested..
+ * @property teams The list of teams of whom a review has been requested.
+ */
 data class GitHubRequestedReviewers(
     val users: Array<GitHubUser>,
     val teams: Array<GitHubTeam>
@@ -157,6 +198,15 @@ data class GitHubRequestedReviewers(
     }
 }
 
+/**
+ * Represents a branch in PR
+ *
+ * @property label The human display name for the merge reference, e.g. "artsy:master".
+ * @property ref The reference point for the merge, e.g. "master".
+ * @property sha The reference point for the merge, e.g. "704dc55988c6996f69b6873c2424be7d1de67bbe".
+ * @property user The user that owns the merge reference e.g. "artsy".
+ * @property repo The repo from which the reference comes from.
+ */
 data class GitHubMergeRef(
     val label: String,
     val ref: String,
@@ -165,6 +215,17 @@ data class GitHubMergeRef(
     val repo: GitHubRepo
 )
 
+/**
+ *  Represents the payload for a repo.
+ *
+ * @property id Generic UUID.
+ * @property name The name of the repo, e.g. "danger-swift".
+ * @property fullName The full name of the owner + repo, e.g. "Danger/danger-swift"
+ * @property isPrivate A boolean stating whether the repo is publicly accessible.
+ * @property description A textual description of the repo.
+ * @property isFork A boolean stating whether the repo is a fork.
+ * @property htmlURL The root web URL for the repo, e.g. https://github.com/artsy/emission
+ */
 data class GitHubRepo(
     val id: Int,
     val name: String,
@@ -174,6 +235,7 @@ data class GitHubRepo(
     @SerializedName("fork") val isFork: Boolean,
     @SerializedName("html_url") val htmlURL: String
 )
+
 
 enum class GitHubReviewState(val value: String) {
     @SerializedName("APPROVED")
@@ -188,6 +250,15 @@ enum class GitHubReviewState(val value: String) {
     DISMISSED("DISMISSED")
 }
 
+/**
+ * Represents the payload for a PR review.
+ *
+ * @property user The user who has completed the review or has been requested to review.
+ * @property id The id for the review (if a review was made).
+ * @property body The body of the review (if a review was made).
+ * @property commitId The commit ID the review was made on (if a review was made).
+ * @property state The state of the review (if a review was made).
+ */
 data class GitHubReview(
     val user: GitHubUser,
     val id: Int?,
@@ -196,6 +267,15 @@ data class GitHubReview(
     val state: GitHubReviewState?
 )
 
+/**
+ * A GitHub specific implementation of a git commit.
+ *
+ * @property sha The SHA for the commit.
+ * @property url The URL for the commit on GitHub.
+ * @property author The GitHub user who wrote the code.
+ * @property commit The raw commit metadata.
+ * @property committer The GitHub user who shipped the code.
+ */
 data class GitHubCommit(
     val sha: String,
     val url: String,
@@ -213,6 +293,32 @@ enum class GitHubIssueState(val value: String) {
     LOCKED("locked")
 }
 
+/**
+ * The GitHub pull request
+ *
+ * @property number The number of the pull request.
+ * @property title The title of the pull request.
+ * @property body The markdown body message of the pull request.
+ * @property user The user who submitted the pull request.
+ * @property assignee The user who submitted the pull request.
+ * @property assignees The users who are assigned to the pull request.
+ * @property createdAt The date for when the pull request was created.
+ * @property updatedAt The date for when the pull request was updated.
+ * @property closedAt The date for when the pull request was closed.
+ * @property mergedAt The date for when the pull request was merged.
+ * @property head The merge reference for the _other_ repo.
+ * @property base The merge reference for the _this_ repo.
+ * @property state The state for the pull request: open, closed, locked, merged.
+ * @property isLocked A boolean indicating if the pull request has been locked to contributors only.
+ * @property isMerged A boolean indicating if the pull request has been merged.
+ * @property commitCount The number of commits in the pull request.
+ * @property commentCount The number of comments in the pull request.
+ * @property reviewCommentCount The number of review-specific comments in the pull request.
+ * @property additions The number of added lines in the pull request.
+ * @property deletions The number of deleted lines in the pull request.
+ * @property changedFiles The number of files changed in the pull request.
+ * @property milestone The milestone of the pull request
+ */
 data class GitHubIssue(
     val id: Int,
     val number: Int,
@@ -275,6 +381,12 @@ data class GitHubIssue(
     }
 }
 
+/**
+ * @property id The id number of this label.
+ * @property url The URL that links to this label.
+ * @property name The name of the label.
+ * @property color TThe color associated with this label.
+ */
 data class GitHubIssueLabel(
         val id: Int,
         val url: String,
@@ -289,6 +401,13 @@ enum class GitHubUserType(val value: String) {
     ORGANIZATION("Organization")
 }
 
+/**
+ * A GitHub user account.
+ *
+ * @property id The UUID for the user organization.
+ * @property login The handle for the user or organization.
+ * @property type The type of user: user or organization.
+ */
 data class GitHubUser(
     val id: Int,
     val login: String,
@@ -304,6 +423,22 @@ enum class GitHubMilestoneState(val value: String) {
     ALL("all")
 }
 
+/**
+ * A GitHub milestone
+ *
+ * @property id The id number of this milestone
+ * @property number The number of this milestone
+ * @property state The state of this milestone: open, closed, all.
+ * @property title The title of this milestone.
+ * @property description The description of this milestone.
+ * @property creator The user who created this milestone.
+ * @property openIssues The number of open issues in this milestone.
+ * @property closedIssues The number of closed issues in this milestone.
+ * @property createdAt The date  for when this milestone was created.
+ * @property updatedAt The date for when the milestone was update.
+ * @property closedAt The date for when the milestone was closed.
+ * @property dueOn The date for the due of this milestone.
+ */
 data class GitHubMilestone(
     val id: Int,
     val number: Int,
