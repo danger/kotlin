4d71798c2d57e175416833e579443cdb6d2e50e6
Franco Meloni <franco.meloni91@gmail.com>
2018-12-14 19:02:05 +0000

    Improve danger js execution log

--- danger-kotlin/src/main/kotlin/DangerJSRunner.kt
+++ danger-kotlin/src/main/kotlin/DangerJSRunner.kt
@@ -4,7 +4,7 @@ fun runDangerJS(command: String, args: List<String>) {
     val argsString = args.joinToString(" ")
     val dangerJSCall = "$(which danger-$command) --process danger-kotlin $argsString"
 
-    println("Executing " + dangerJSCall)
+    "echo \"Executing $dangerJSCall\"".exec()
 
     dangerJSCall.exec()
 }
