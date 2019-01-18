3819dadb22bb1514dbc21d7cb9d645288f3253dd
Franco Meloni <franco.meloni91@gmail.com>
2019-01-18 21:40:56 +0000

    Remove generated filed after dangerfile execution

--- danger-kotlin/src/main/kotlin/Runner.kt
+++ danger-kotlin/src/main/kotlin/Runner.kt
@@ -2,6 +2,7 @@ import platform.posix.*
 
 const val TMP_INPUT_JSON_FILE = "danger_in.json"
 const val TMP_OUT_JSON_FILE = "danger_out.json"
+const val EXECUTABLE_DANGERFILE_NAME = "Dangerfile"
 
 fun runDangerKotlin() {
     fopen(TMP_INPUT_JSON_FILE, "wt")?.apply {
@@ -15,6 +16,9 @@ fun runDangerKotlin() {
     }
 
     "kscript --package Dangerfile.kts".exec()
-    "./Dangerfile $TMP_INPUT_JSON_FILE $TMP_OUT_JSON_FILE".exec()
+    "./$EXECUTABLE_DANGERFILE_NAME $TMP_INPUT_JSON_FILE $TMP_OUT_JSON_FILE".exec()
     "echo danger-results:/`pwd`/$TMP_OUT_JSON_FILE".exec()
+
+    remove(TMP_INPUT_JSON_FILE)
+    remove(EXECUTABLE_DANGERFILE_NAME)
 }
