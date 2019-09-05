908518c016b6d2a1a7f34e29e1986c77099ad387
Franco Meloni <franco.meloni91@gmail.com>
2019-09-05 22:53:23 +0100

    Remove additional kscript references

--- danger-kotlin/src/runnerMain/kotlin/com/danger/runner/cmd/dangerfile/DangerFileBridge.kt
+++ danger-kotlin/src/runnerMain/kotlin/com/danger/runner/cmd/dangerfile/DangerFileBridge.kt
@@ -1,10 +1,6 @@
 package com.danger.runner.cmd.dangerfile
 
-import com.danger.runner.cmd.kscript.KScriptBridge
-
 interface DangerFileBridge {
-    val kscript: KScriptBridge
-
     fun edit()
     fun execute(inputJson: String, outputJson: String)
 }
