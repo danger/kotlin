fca9338e79c046be6d8b4293afe7ed5e81a75de3
Gianluca Zuddas <gianluca.zuddas@gmail.com>
2019-09-07 20:15:13 +0100

    Update script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 if ! [ -x "$(command -v kotlinc)" ]; then
     echo "Installing kotlin compiler 1.3.50"
-    curl -O -L https://github.com/JetBrains/kotlin/releases/download/v1.3.50/kotlin-compiler-1.3.50.zip
+    curl -o kotlin-compiler.zip -L https://github.com/JetBrains/kotlin/releases/download/v1.3.50/kotlin-compiler-1.3.50.zip
     mkdir /opt/kotlin
-    unzip -d /opt/kotlin kotlin-compiler-1.3.50.zip
+    unzip -d /opt/kotlin kotlin-compiler.zip
     export PATH=$PATH:/opt/kotlin
-    rm -rf kotlin-compiler-1.3.50.zip
+    rm -rf kotlin-compiler.zip
 fi
 if ! [ -x "$(command -v gradle)" ]; then
     echo "Installing gradle 5.6.2"
-    curl -O -L https://downloads.gradle-dn.com/distributions/gradle-5.6.2-bin.zip
+    curl -o gradle.zip -L https://downloads.gradle-dn.com/distributions/gradle-5.6.2-bin.zip
     mkdir /opt/gradle
-    unzip -d /opt/gradle gradle-5.6.2-bin.zip
+    unzip -d /opt/gradle gradle.zip
     export PATH=$PATH:/opt/gradle/gradle-5.6.2/bin
-    rm -rf gradle-5.6.2-bin.zip
+    rm -rf gradle.zip
 fi
 git clone https://github.com/danger/kotlin.git _danger-kotlin
 sudo chmod -R a+rwx /usr/local/
