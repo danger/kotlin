e4ebfc81754b553146df111751abec31cdf6f4d1
Gianluca Zuddas <gianluca.zuddas@gmail.com>
2019-09-07 19:55:21 +0100

    Updated install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if ! [ -x "$(command -v kotlinc)" ]; then
     echo "Installing kotlin compiler 1.3.50"
-    curl https://github.com/JetBrains/kotlin/releases/download/v1.3.50/kotlin-compiler-1.3.50.zip
+    curl -L https://github.com/JetBrains/kotlin/releases/download/v1.3.50/kotlin-compiler-1.3.50.zip
     mkdir /opt/kotlin
     unzip -d /opt/kotlin kotlin-compiler-1.3.50.zip
     export PATH=$PATH:/opt/kotlin
@@ -9,7 +9,7 @@ if ! [ -x "$(command -v kotlinc)" ]; then
 fi
 if ! [ -x "$(command -v gradle)" ]; then
     echo "Installing gradle 5.6.2"
-    curl -q https://downloads.gradle-dn.com/distributions/gradle-5.6.2-bin.zip
+    curl -L https://downloads.gradle-dn.com/distributions/gradle-5.6.2-bin.zip
     mkdir /opt/gradle
     unzip -d /opt/gradle gradle-5.6.2-bin.zip
     export PATH=$PATH:/opt/gradle/gradle-5.6.2/bin
