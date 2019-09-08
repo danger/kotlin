51b78644d07dabd15d130c70199ecbe3a4b1be27
Franco Meloni <franco.meloni91@gmail.com>
2019-09-08 10:30:54 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@ if ! [[ -x "$(command -v kotlinc)" ]]; then
     echo "Installing kotlin compiler 1.3.50"
     curl -o kotlin-compiler.zip -L https://github.com/JetBrains/kotlin/releases/download/v1.3.50/kotlin-compiler-1.3.50.zip
     unzip -d /usr/local/ kotlin-compiler.zip
-    export PATH=/usr/local/kotlinc/bin:$PATH
+    echo 'PATH=/usr/local/kotlinc/bin:$PATH' >> ~/.bash_profile
     rm -rf kotlin-compiler.zip
 fi
 if ! [[ -x "$(command -v gradle)" ]]; then
@@ -12,7 +12,7 @@ if ! [[ -x "$(command -v gradle)" ]]; then
     curl -o gradle.zip -L https://downloads.gradle-dn.com/distributions/gradle-5.6.2-bin.zip
     mkdir /opt/gradle
     unzip -d /opt/gradle gradle.zip
-    export PATH=/opt/gradle/gradle-5.6.2/bin:$PATH
+    echo 'export PATH=/opt/gradle/gradle-5.6.2/bin:$PATH' >> ~/.bash_profile
     rm -rf gradle.zip
 fi
 git clone https://github.com/danger/kotlin.git _danger-kotlin
