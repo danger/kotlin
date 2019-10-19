789184402058c16ca3e233b59664f246ad5b6f2f
Gianluca Zuddas <gianluca.zuddas@gmail.com>
2019-10-19 16:48:40 +0100

    Restore dangerfile

--- Dangerfile.main.kts
+++ Dangerfile.main.kts
@@ -1,29 +1,27 @@
 // Dangerfile.main.kts
 // Using external dependencies:
-@file:Repository("https://repo.maven.apache.org")
-@file:DependsOn("org.apache.commons:commons-text:1.6")
+//@file:Repository("https://repo.maven.apache.org")
+//@file:DependsOn("org.apache.commons:commons-text:1.6")
 
-println("Dangerfile")
+import com.danger.dangerkotlin.*
+import org.jetbrains.kotlin.script.util.*
 
-//import com.danger.dangerkotlin.*
-//import org.jetbrains.kotlin.script.util.*
-//
-//val danger = Danger(args)
-//
-//val allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
-//
-//val changelogChanged = allSourceFiles.contains("CHANGELOG.md")
-//val sourceChanges = allSourceFiles.firstOrNull { it.contains("src")  }
-//val isTrivial = danger.github!!.pullRequest.title.contains("#trivial")
-//
-//if (!isTrivial && !changelogChanged && sourceChanges != null) {
-//    warn("Any changes to library code should be reflected in the Changelog.\n\nPlease consider adding a note there and adhere to the [Changelog Guidelines](https://github.com/Moya/contributors/blob/master/Changelog%20Guidelines.md).")
-//}
-//
-//if (danger.git.createdFiles.size + danger.git.modifiedFiles.size - danger.git.deletedFiles.size > 10) {
-//    warn("Big PR, try to keep changes smaller if you can")
-//}
-//
-//if (danger.github!!.pullRequest.title.contains("WIP" ,false)) {
-//    warn("PR is classed as Work in Progress")
-//}
+val danger = Danger(args)
+
+val allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
+
+val changelogChanged = allSourceFiles.contains("CHANGELOG.md")
+val sourceChanges = allSourceFiles.firstOrNull { it.contains("src")  }
+val isTrivial = danger.github!!.pullRequest.title.contains("#trivial")
+
+if (!isTrivial && !changelogChanged && sourceChanges != null) {
+    warn("Any changes to library code should be reflected in the Changelog.\n\nPlease consider adding a note there and adhere to the [Changelog Guidelines](https://github.com/Moya/contributors/blob/master/Changelog%20Guidelines.md).")
+}
+
+if (danger.git.createdFiles.size + danger.git.modifiedFiles.size - danger.git.deletedFiles.size > 10) {
+    warn("Big PR, try to keep changes smaller if you can")
+}
+
+if (danger.github!!.pullRequest.title.contains("WIP" ,false)) {
+    warn("PR is classed as Work in Progress")
+}
