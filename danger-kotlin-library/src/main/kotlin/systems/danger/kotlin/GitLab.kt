ee43001dac574daeb1e634b29189ec0c04dc2fa6
Franco Meloni <franco.meloni91@gmail.com>
2020-01-26 19:29:57 +0000

    Add gitlab support

---
+++ danger-kotlin-library/src/main/kotlin/systems/danger/kotlin/GitLab.kt
@@ -0,0 +1,286 @@
+package systems.danger.kotlin
+
+import com.squareup.moshi.Json
+import java.util.*
+import kotlin.collections.ArrayList
+
+data class GitLab(
+    @Json(name="mr")
+    val mergeRequest: GitLabMergeRequest,
+    val metadata: GitLabMetadata
+)
+
+data class GitLabDiffRefs (
+    @Json(name="base_sha")
+    val baseSha: String,
+    @Json(name="head_sha")
+    val headSha: String,
+    @Json(name="start_sha")
+    val startSha: String
+)
+
+data class GitLabUserMergeData(
+    @Json(name="can_merge")
+    val canMerge: Boolean
+)
+
+data class GitLabMergeRequest(
+    @Json(name="allow_collaboration")
+    val allowCollaboration: Boolean?,
+    @Json(name="allow_maintainer_to_push")
+    val allowMaintainerToPush: Boolean?,
+    @Json(name="approvals_before_merge")
+    val approvalsBeforeMerge: Int?,
+    val assignee: GitLabUser?,
+    val author: GitLabUser,
+    @Json(name="changes_count")
+    val changesCount: String,
+    @Json(name="closed_at")
+    val closedAt: Date?,
+    @Json(name="closed_by")
+    val closedBy: GitLabUser?,
+    val description: String,
+    @Json(name="diff_refs")
+    val diffRefs: GitLabDiffRefs,
+    val downvotes: Int,
+    @Json(name="first_deployed_to_production_at")
+    val firstDeployedToProductionAt: Date?,
+    @Json(name="force_remove_source_branch")
+    val forceRemoveSourceBranch: Boolean,
+    val id: Int,
+    val iid: Int,
+    @Json(name="latest_build_finished_at")
+    val latestBuildFinishedAt: Date?,
+    @Json(name="latest_build_started_at")
+    val latestBuildStartedAt: Date?,
+    val labels: Array<String>,
+    @Json(name="merge_commit_sha")
+    val mergeCommitSha: String?,
+    @Json(name="merged_at")
+    val mergedAt: Date?,
+    @Json(name="merged_by")
+    val mergedBy: GitLabUser?,
+    @Json(name="merge_when_pipeline_succeeds")
+    val mergeOnPipelineSuccess: Boolean,
+    val milestone: GitLabMilestone?,
+    val pipeline: GitLabPipeline,
+    @Json(name="project_id")
+    val projectId: String,
+    val sha: String,
+    @Json(name="should_remove_source_branch")
+    val shouldRemoveSourceBranch: Boolean?,
+    @Json(name="source_branch")
+    val sourceBranch: String,
+    @Json(name="source_project_id")
+    val sourceProjectId: String,
+    val state: GitLabMergeRequestState,
+    val subscribed: Boolean,
+    @Json(name="target_branch")
+    val targetBranch: String,
+    @Json(name="target_project_id")
+    val targetProjectId: String,
+    val timeStats: GitLabMergeRequestTimeStats?,
+    val title: String,
+    val upvotes: Int,
+    @Json(name="user")
+    private val userMergeData: GitLabUserMergeData,
+    @Json(name="user_notes_count")
+    val userNotesCount: Int,
+    @Json(name="web_url")
+    val webUrl: String,
+    @Json(name="work_in_progress")
+    val workInProgress: Boolean
+) {
+    val canMerge: Boolean
+        get() = this.userMergeData.canMerge
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as GitLabMergeRequest
+
+        if (allowCollaboration != other.allowCollaboration) return false
+        if (allowMaintainerToPush != other.allowMaintainerToPush) return false
+        if (approvalsBeforeMerge != other.approvalsBeforeMerge) return false
+        if (assignee != other.assignee) return false
+        if (author != other.author) return false
+        if (changesCount != other.changesCount) return false
+        if (closedAt != other.closedAt) return false
+        if (closedBy != other.closedBy) return false
+        if (description != other.description) return false
+        if (diffRefs != other.diffRefs) return false
+        if (downvotes != other.downvotes) return false
+        if (firstDeployedToProductionAt != other.firstDeployedToProductionAt) return false
+        if (forceRemoveSourceBranch != other.forceRemoveSourceBranch) return false
+        if (id != other.id) return false
+        if (iid != other.iid) return false
+        if (latestBuildFinishedAt != other.latestBuildFinishedAt) return false
+        if (latestBuildStartedAt != other.latestBuildStartedAt) return false
+        if (!labels.contentEquals(other.labels)) return false
+        if (mergeCommitSha != other.mergeCommitSha) return false
+        if (mergedAt != other.mergedAt) return false
+        if (mergedBy != other.mergedBy) return false
+        if (mergeOnPipelineSuccess != other.mergeOnPipelineSuccess) return false
+        if (milestone != other.milestone) return false
+        if (pipeline != other.pipeline) return false
+        if (projectId != other.projectId) return false
+        if (sha != other.sha) return false
+        if (shouldRemoveSourceBranch != other.shouldRemoveSourceBranch) return false
+        if (sourceBranch != other.sourceBranch) return false
+        if (sourceProjectId != other.sourceProjectId) return false
+        if (state != other.state) return false
+        if (subscribed != other.subscribed) return false
+        if (targetBranch != other.targetBranch) return false
+        if (targetProjectId != other.targetProjectId) return false
+        if (timeStats != other.timeStats) return false
+        if (userMergeData != other.userMergeData) return false
+        if (title != other.title) return false
+        if (upvotes != other.upvotes) return false
+        if (userNotesCount != other.userNotesCount) return false
+        if (webUrl != other.webUrl) return false
+        if (workInProgress != other.workInProgress) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = allowCollaboration.hashCode()
+        result = 31 * result + allowMaintainerToPush.hashCode()
+        result = 31 * result + approvalsBeforeMerge.hashCode()
+        result = 31 * result + (assignee?.hashCode() ?: 0)
+        result = 31 * result + author.hashCode()
+        result = 31 * result + changesCount.hashCode()
+        result = 31 * result + closedAt.hashCode()
+        result = 31 * result + closedBy.hashCode()
+        result = 31 * result + description.hashCode()
+        result = 31 * result + diffRefs.hashCode()
+        result = 31 * result + downvotes
+        result = 31 * result + (firstDeployedToProductionAt?.hashCode() ?: 0)
+        result = 31 * result + forceRemoveSourceBranch.hashCode()
+        result = 31 * result + id
+        result = 31 * result + iid
+        result = 31 * result + latestBuildFinishedAt.hashCode()
+        result = 31 * result + latestBuildStartedAt.hashCode()
+        result = 31 * result + labels.contentHashCode()
+        result = 31 * result + (mergeCommitSha?.hashCode() ?: 0)
+        result = 31 * result + (mergedAt?.hashCode() ?: 0)
+        result = 31 * result + (mergedBy?.hashCode() ?: 0)
+        result = 31 * result + mergeOnPipelineSuccess.hashCode()
+        result = 31 * result + (milestone?.hashCode() ?: 0)
+        result = 31 * result + pipeline.hashCode()
+        result = 31 * result + projectId.hashCode()
+        result = 31 * result + sha.hashCode()
+        result = 31 * result + (shouldRemoveSourceBranch?.hashCode() ?: 0)
+        result = 31 * result + sourceBranch.hashCode()
+        result = 31 * result + sourceProjectId.hashCode()
+        result = 31 * result + state.hashCode()
+        result = 31 * result + subscribed.hashCode()
+        result = 31 * result + targetBranch.hashCode()
+        result = 31 * result + targetProjectId.hashCode()
+        result = 31 * result + timeStats.hashCode()
+        result = 31 * result + userMergeData.hashCode()
+        result = 31 * result + title.hashCode()
+        result = 31 * result + upvotes
+        result = 31 * result + userNotesCount
+        result = 31 * result + webUrl.hashCode()
+        result = 31 * result + workInProgress.hashCode()
+        return result
+    }
+}
+
+data class GitLabMergeRequestTimeStats(
+    @Json(name="human_time_estimate")
+    val humanTimeEstimate: Int?,
+    @Json(name="human_time_spent")
+    val humanTimeSpent: Int?,
+    @Json(name="time_estimate")
+    val timeEstimate: Int,
+    @Json(name="total_time_spent")
+    val totalTimeSpent: Int
+)
+
+data class GitLabMetadata(
+    val pullRequestID: String,
+    val repoSlug: String
+)
+
+enum class GitLabMergeRequestState {
+    @Json(name = "closed")
+    closed,
+    @Json(name = "locked")
+    locked,
+    @Json(name = "merged")
+    merged,
+    @Json(name = "opened")
+    opened
+}
+
+data class GitLabMilestone(
+    @Json(name="created_at")
+    val createdAt: Date,
+    val description: String,
+    @Json(name="due_date")
+    val dueDate: Date,
+    val id: Int,
+    val iid: Int,
+    @Json(name="project_id")
+    val projectID: Int,
+    @Json(name="start_date")
+    val startDate: Date,
+    val state: GitLabMilestoneState,
+    val title: String,
+    @Json(name="updated_at")
+    val updatedAt: Date,
+    @Json(name="web_url")
+    val webUrl: String
+)
+
+enum class GitLabMilestoneState {
+    @Json(name = "active")
+    active,
+    @Json(name = "closed")
+    closed
+}
+
+data class GitLabPipeline(
+    val id: Int,
+    val ref: String,
+    val sha: String,
+    val status: GitLabPapelineStatus,
+    @Json(name="web_url")
+    val webUrl: String
+)
+
+enum class GitLabPapelineStatus {
+    @Json(name = "cancelled")
+    cancelled,
+    @Json(name = "failed")
+    failed,
+    @Json(name = "pending")
+    pending,
+    @Json(name = "running")
+    running,
+    @Json(name = "skipped")
+    skipped,
+    @Json(name = "success")
+    success
+}
+
+data class GitLabUser(
+    @Json(name="avatar_url")
+    val avatarUrl: String?,
+    val id: Int,
+    val name: String,
+    val state: GitLabUserState,
+    val username: String,
+    @Json(name="web_url")
+    val webUrl: String
+)
+
+enum class GitLabUserState {
+    @Json(name = "active")
+    active,
+    @Json(name = "blocked")
+    blocked
+}
---
+++ danger-kotlin-library/src/test/kotlin/systems/danger/kotlin/GitLabParsingTests.kt
@@ -0,0 +1,77 @@
+package systems.danger.kotlin
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import org.junit.Assert.*
+import org.junit.Test
+import java.util.*
+
+class GitLabParsingTests {
+    private val jsonFiles = JSONFiles()
+    private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).add(Date::class.java, Rfc3339DateJsonAdapter().nullSafe()).build().adapter(
+        DSL::class.java)
+    private val dsl
+        get() = moshi.fromJson(jsonFiles.gitlabJSON)
+    private val gitLab: GitLab
+        get() = dsl!!.danger.gitlab
+
+    @Test
+    fun testItParsesTheGitLabMergeRequest() {
+        with(gitLab.mergeRequest) {
+            assertEquals(false, allowCollaboration)
+            assertEquals(false, allowMaintainerToPush)
+            assertEquals(1, approvalsBeforeMerge)
+            val orta = GitLabUser("https://secure.gravatar.com/avatar/f116cb3be23153ec08b94e8bd4dbcfeb?s=80&d=identicon", 377669, "Orta",
+                GitLabUserState.active, "orta", "https://gitlab.com/orta")
+            assertEquals(orta, assignee)
+            val fmeloni = GitLabUser("https://secure.gravatar.com/avatar/3d90e967de2beab6d44cfadbb4976b87?s=80&d=identicon", 3331525, "Franco Meloni", GitLabUserState.active, "f-meloni", "https://gitlab.com/f-meloni")
+            assertEquals(fmeloni, author)
+            assertEquals(false, canMerge)
+            assertEquals("1", changesCount)
+            assertEquals(null, closedAt)
+            assertEquals(null, closedBy)
+            assertEquals("Updating it to avoid problems like https://github.com/danger/swift/issues/221", description)
+            val expectedDiffRefs = GitLabDiffRefs("ef28580bb2a00d985bffe4a4ce3fe09fdb12283f", "621bc3348549e51c5bd6ea9f094913e9e4667c7b", "ef28580bb2a00d985bffe4a4ce3fe09fdb12283f")
+            assertEquals(expectedDiffRefs, diffRefs)
+            assertEquals(0, downvotes)
+            assertEquals(Date(1554939022492), firstDeployedToProductionAt)
+            assertEquals(true, forceRemoveSourceBranch)
+            assertEquals(27469633, id)
+            assertEquals(182, iid)
+            assertEquals(Date(1554939202492), latestBuildFinishedAt)
+            assertEquals(Date(1554938422492), latestBuildStartedAt)
+            assertArrayEquals(arrayOf(), labels)
+            assertEquals("377a24fb7a0f30364f089f7bca67752a8b61f477", mergeCommitSha)
+            assertEquals(Date(1554939442492), mergedAt)
+            assertEquals(orta, mergedBy)
+            assertEquals(false, mergeOnPipelineSuccess)
+            val expectedMilestone = GitLabMilestone(Date(1554929865346), "Test Description",Date(1560121200000), 1,  2, 1000, Date(1554929865346), GitLabMilestoneState.closed, "Test Milestone", Date(1554929865346), "https://gitlab.com/milestone")
+            assertEquals(expectedMilestone, milestone)
+            val expectedPipeline = GitLabPipeline(50, "ef28580bb2a00d985bffe4a4ce3fe09fdb12283f", "621bc3348549e51c5bd6ea9f094913e9e4667c7b", GitLabPapelineStatus.success, "https://gitlab.com/danger-systems/danger.systems/pipeline/621bc3348549e51c5bd6ea9f094913e9e4667c7b")
+            assertEquals(expectedPipeline, pipeline)
+            assertEquals("1620437", projectId)
+            assertEquals("621bc3348549e51c5bd6ea9f094913e9e4667c7b", sha)
+            assertEquals(null, shouldRemoveSourceBranch)
+            assertEquals("patch-2", sourceBranch)
+            assertEquals("10132593", sourceProjectId)
+            assertEquals(GitLabMergeRequestState.merged, state)
+            assertEquals(false, subscribed)
+            assertEquals("master", targetBranch)
+            assertEquals("1620437", targetProjectId)
+            assertEquals(null, timeStats)
+            assertEquals("Update getting_started.html.slim", title)
+            assertEquals(0, upvotes)
+            assertEquals(0, userNotesCount)
+            assertEquals("https://gitlab.com/danger-systems/danger.systems/merge_requests/182", webUrl)
+            assertEquals(false, workInProgress)
+        }
+    }
+
+    @Test
+    fun testItParsesTheGitLabMetadata() {
+        with(gitLab.metadata) {
+            assertEquals("182", pullRequestID)
+            assertEquals("danger-systems/danger.systems", repoSlug)
+        }
+    }
+}
