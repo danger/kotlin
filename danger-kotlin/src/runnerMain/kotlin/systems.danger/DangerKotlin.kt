0f71d1ca95b186aa9c8c325cceba7ad994399883
Gianluca Zuddas <gianluca.zuddas@gmail.com>
2020-01-26 22:29:30 +0000

    Strip not wanted endline

--- danger-kotlin/src/runnerMain/kotlin/systems.danger/DangerKotlin.kt
+++ danger-kotlin/src/runnerMain/kotlin/systems.danger/DangerKotlin.kt
@@ -8,7 +8,7 @@ object DangerKotlin {
     fun run() {
         val dangerDSLPath = readLine()
 
-        dangerDSLPath?.removePrefix("danger://dsl/")?.removeNonPrintableChars()?.let {
+        dangerDSLPath?.removePrefix("danger://dsl/")?.stripEndLine()?.let {
             with(DangerFile) {
                 execute(it, FILE_TMP_OUTPUT_JSON)
             }
@@ -21,6 +21,6 @@ object DangerKotlin {
         println("danger-results:/$FILE_TMP_OUTPUT_JSON")
     }
 
-    private fun String.removeNonPrintableChars() = replace("\\p{C}", "")
+    private fun String.stripEndLine() = replace("\u007F", "")
 
 }
