90afbd393345ef1377ddb7021b4dc85a1cf0bb22
Gianluca Zuddas <gianluca.zuddas@gmail.com>
2020-04-28 18:55:46 +0100

    Updated dangerfile using new extensions

--- Dangerfile.df.kts
+++ Dangerfile.df.kts
@@ -1,5 +1,4 @@
 // Dangerfile.df.kts
-
 /*
  * Use external dependencies using the following annotations:
  */
@@ -14,23 +13,29 @@ import systems.danger.kotlin.*
 danger(args) {
 
     val allSourceFiles = git.modifiedFiles + git.createdFiles
-
     val changelogChanged = allSourceFiles.contains("CHANGELOG.md")
     val sourceChanges = allSourceFiles.firstOrNull { it.contains("src") }
-    val isTrivial = github.pullRequest.title.contains("#trivial")
 
-    if (!isTrivial && !changelogChanged && sourceChanges != null) {
-        warn(WordUtils.capitalize("any changes to library code should be reflected in the Changelog.\n\nPlease consider adding a note there and adhere to the [Changelog Guidelines](https://github.com/Moya/contributors/blob/master/Changelog%20Guidelines.md)."))
+    onGitHub {
+        val isTrivial = pullRequest.title.contains("#trivial")
+
+        // Changelog
+        if (!isTrivial && !changelogChanged && sourceChanges != null) {
+            warn(WordUtils.capitalize("any changes to library code should be reflected in the Changelog.\n\nPlease consider adding a note there and adhere to the [Changelog Guidelines](https://github.com/Moya/contributors/blob/master/Changelog%20Guidelines.md)."))
+        }
+
+        // Big PR Check
+        if ((pullRequest.additions ?: 0) - (pullRequest.deletions ?: 0) > 300) {
+            warn("Big PR, try to keep changes smaller if you can")
+        }
+
+        // Work in progress check
+        if (pullRequest.title.contains("WIP", false)) {
+            warn("PR is classed as Work in Progress")
+        }
     }
 
-    if ((github.pullRequest.additions ?: 0) - (github.pullRequest.deletions ?: 0) > 300) {
-        warn("Big PR, try to keep changes smaller if you can")
-    }
-
-    if (github.pullRequest.title.contains("WIP", false)) {
-        warn("PR is classed as Work in Progress")
-    }
-
+    //No Java files check
     git.createdFiles.filter {
         it.endsWith(".java")
     }.forEach {
