be7d84d473635cd4ff673f4e7c15e1713f05ce39
Franco Meloni <franco.meloni91@gmail.com>
2020-05-23 19:24:56 +0100

    Add exec utils function

--- danger-kotlin-library/src/main/kotlin/systems/danger/kotlin/Utils.kt
+++ danger-kotlin-library/src/main/kotlin/systems/danger/kotlin/Utils.kt
@@ -6,4 +6,13 @@ class Utils {
     fun readFile(path: String): String {
         return File(path).readText()
     }
+
+    fun exec(command: String, arguments: Array<String> = arrayOf()): String {
+        var commandToExec = "/bin/bash -c $command" + if (arguments.isNotEmpty()) "" else " " + arguments.joinToString(" ")
+
+        val process = Runtime.getRuntime().exec(commandToExec)
+        process.waitFor()
+
+        return process.inputStream.bufferedReader().readText()
+    }
 }
