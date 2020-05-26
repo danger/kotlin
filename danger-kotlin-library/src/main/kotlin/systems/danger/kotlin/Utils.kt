f11e56c877cf669d6fd67eadc097841782c0700f
Franco Meloni <franco.meloni91@gmail.com>
2020-05-26 21:46:36 +0100

    Add Javadoc for exec function

--- danger-kotlin-library/src/main/kotlin/systems/danger/kotlin/Utils.kt
+++ danger-kotlin-library/src/main/kotlin/systems/danger/kotlin/Utils.kt
@@ -3,10 +3,18 @@ package systems.danger.kotlin
 import java.io.File
 
 class Utils {
+
     fun readFile(path: String): String {
         return File(path).readText()
     }
 
+    /**
+     * Gives you the ability to cheaply run a command and read the output without having to mess around
+     *
+     * @param command The first part of the command
+     * @param arguments An optional array of arguements to pass in extra
+     * @return the stdout from the command
+     */
     fun exec(command: String, arguments: Array<String> = arrayOf()): String {
         val commandToExec = "/bin/bash -c $command" + if (arguments.isNotEmpty())  " " + arguments.joinToString(" ") else ""
 
