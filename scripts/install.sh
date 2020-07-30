ba227509340d7f042cba85ec168ec59310ce06c5
Anton Nurdin Tuhadiansyah <anton.work19@gmail.com>
2020-07-30 17:54:21 +0700

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -38,7 +38,7 @@ if ! [[ -x "$(command -v gradle)" ]]; then
     rm -rf gradle.zip
 fi
 
-git clone https://github.com/danger/kotlin.git --single-branch --depth 1 _danger-kotlin
+git clone https://github.com/anton46/kotlin-1.git --single-branch --depth 1 _danger-kotlin
 cd _danger-kotlin && make install
 cd ..
 rm -rf _danger-kotlin
