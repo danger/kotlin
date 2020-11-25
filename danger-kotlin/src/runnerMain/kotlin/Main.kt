136e1a4f2bbc01bf35b8fff42aa2d216bf2f926f
Gianluca Zuddas <gianluca.zuddas@gmail.com>
2020-11-25 12:18:49 +0000

    Fixed some core indentation

--- danger-kotlin/src/runnerMain/kotlin/Main.kt
+++ danger-kotlin/src/runnerMain/kotlin/Main.kt
@@ -13,7 +13,7 @@ fun main(args: Array<String>) {
     if (args.isNotEmpty()) {
         when (val command = args.first()) {
             "ci", "local", "pr" -> {
-                DangerJS.process(command ,PROCESS_DANGER_KOTLIN, args.drop(1))
+                DangerJS.process(command, PROCESS_DANGER_KOTLIN, args.drop(1))
             }
             "runner" -> DangerKotlin.run()
             "--version" -> Log.info(VERSION)
