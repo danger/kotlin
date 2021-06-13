45a0bb661465bcd246b9d4f0bb7a0401360cbb0e
Gianluca Zuddas <gianluca.zuddas@gmail.com>
2021-06-13 17:48:14 +0100

    Coroutines compatibility test

--- Dangerfile_ci.df.kts
+++ Dangerfile_ci.df.kts
@@ -8,9 +8,14 @@
 //Testing plugin
 @file:DependsOn("danger-kotlin-sample-plugin-sample.jar")
 
+import kotlinx.coroutines.runBlocking
+import kotlinx.datetime.Clock
+import kotlinx.datetime.toDateTimePeriod
 import org.apache.commons.text.WordUtils
 import systems.danger.kotlin.*
+import systems.danger.kotlin.models.danger.DangerDSL
 import systems.danger.samples.plugin.SamplePlugin
+import kotlin.time.Duration
 
 register plugin SamplePlugin
 
@@ -49,4 +54,21 @@ danger(args) {
             warn(WordUtils.capitalize("please consider to create new files in Kotlin"), it, 1)
         }
     }
+
+    // Coroutines checks in parallel test
+    val current = Clock.System.now()
+    runBlocking {
+        expensiveCheck(1000)
+        expensiveCheck(3000)
+        expensiveCheck(2000)
+        expensiveCheck(5000)
+    }
+    val after = Clock.System.now()
+    val runningTime = after.minus(current)
+    message("Coroutines checks terminated in ${runningTime.toDateTimePeriod().seconds} seconds.")
+}
+
+suspend fun DangerDSL.expensiveCheck(runForMillis: Long) {
+    // Example expensive check
+    Thread.sleep(runForMillis)
 }
