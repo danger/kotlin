69a31a8b4d086909fad8048bf2e8a19e98be099b
Rojan Thomas <rojan.thomas@bglgroup.co.uk>
2021-07-22 12:52:17 +0100

    Improving error message for when a DangerPlugin was not registered

--- danger-kotlin-sdk/src/main/kotlin/systems/danger/kotlin/sdk/DangerKotlinAPI.kt
+++ danger-kotlin-sdk/src/main/kotlin/systems/danger/kotlin/sdk/DangerKotlinAPI.kt
@@ -146,6 +146,21 @@ abstract class DangerPlugin {
     // The plugin unique id
     abstract val id: String
 
-    // The DangerContext
-    lateinit var context: DangerContext
+    /**
+     * The context that will be set by the danger runner. This wil be null if the plugin has not been registered in the
+     * Dangerfile.
+     */
+    var registeredContext : DangerContext? = null
+
+    /**
+     * Provides a backing getter around registeredContext to avoid null checks if we know the plugin must be registered
+     * in the Dangerfile.
+     *
+     * @throws NullPointerException if the plugin was not registered in the Dangerfile.
+     */
+    val context : DangerContext
+        get() = registeredContext ?: throw NullPointerException(
+            "DangerContext is null! Have you registered this plugin in your Dangerfile e.g. " +
+                    "'register plugin ${this::class.simpleName}'?"
+        )
 }
