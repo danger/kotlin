303a6b44782ba450ef5ef148d806ee5e6fb89bec
Gianluca Zuddas <gianluca.zuddas@gmail.com>
2021-12-07 22:52:11 +0000

    Using MavenDependencyResolver instead of Ivy

--- danger-kotlin-kts/src/main/kotlin/systems/danger/kts/DangerFileScriptDefinition.kt
+++ danger-kotlin-kts/src/main/kotlin/systems/danger/kts/DangerFileScriptDefinition.kt
@@ -11,6 +11,7 @@ import kotlin.script.experimental.dependencies.*
 import kotlin.script.experimental.dependencies.maven.MavenDependenciesResolver
 import kotlin.script.experimental.host.FileBasedScriptSource
 import kotlin.script.experimental.host.FileScriptSource
+import kotlin.script.experimental.impl.internalScriptingRunSuspend
 import kotlin.script.experimental.jvm.compat.mapLegacyDiagnosticSeverity
 import kotlin.script.experimental.jvm.compat.mapLegacyScriptPosition
 import kotlin.script.experimental.jvm.dependenciesFromClassContext
@@ -64,13 +65,12 @@ class DangerFileKtsConfigurator : RefineScriptCompilationConfigurationHandler {
 
     private val resolver = CompoundDependenciesResolver(
         FileSystemDependenciesResolver(DANGER_LIBS_FLAT_DIR),
-        MavenDependenciesResolver()
-    )
+        MavenDependenciesResolver())
 
     override operator fun invoke(context: ScriptConfigurationRefinementContext): ResultWithDiagnostics<ScriptCompilationConfiguration> =
         processAnnotations(context)
 
-    private fun processAnnotations(context: ScriptConfigurationRefinementContext): ResultWithDiagnostics<ScriptCompilationConfiguration> {
+    fun processAnnotations(context: ScriptConfigurationRefinementContext): ResultWithDiagnostics<ScriptCompilationConfiguration> {
         val diagnostics = arrayListOf<ScriptDiagnostic>()
 
         fun report(severity: ScriptDependenciesResolver.ReportSeverity, message: String, position: ScriptContents.Position?) {
@@ -89,28 +89,44 @@ class DangerFileKtsConfigurator : RefineScriptCompilationConfigurationHandler {
             ?: return context.compilationConfiguration.asSuccess()
 
         val scriptBaseDir = (context.script as? FileBasedScriptSource)?.file?.parentFile
-        val importedSources = annotations.filterByAnnotationType<Import>().flatMap {
-            it.annotation.paths.map { sourceName ->
-                FileScriptSource(scriptBaseDir?.resolve(sourceName) ?: File(sourceName))
+        val importedSources = linkedMapOf<String, Pair<File, String>>()
+        var hasImportErrors = false
+        annotations.filterByAnnotationType<Import>().forEach { scriptAnnotation ->
+            scriptAnnotation.annotation.paths.forEach { sourceName ->
+                val file = (scriptBaseDir?.resolve(sourceName) ?: File(sourceName)).normalize()
+                val keyPath = file.absolutePath
+                val prevImport = importedSources.put(keyPath, file to sourceName)
+                if (prevImport != null) {
+                    diagnostics.add(
+                        ScriptDiagnostic(
+                            ScriptDiagnostic.unspecifiedError, "Duplicate imports: \"${prevImport.second}\" and \"$sourceName\"",
+                            sourcePath = context.script.locationId, location = scriptAnnotation.location?.locationInText
+                        )
+                    )
+                    hasImportErrors = true
+                }
             }
         }
+        if (hasImportErrors) return ResultWithDiagnostics.Failure(diagnostics)
+
         val compileOptions = annotations.filterByAnnotationType<CompilerOptions>().flatMap {
             it.annotation.options.toList()
         }
 
         val resolveResult = try {
-            runBlocking {
-                resolver.resolveFromScriptSourceAnnotations(annotations.filter { it.annotation is DependsOn || it.annotation is Repository })
-            }
-
+            @Suppress("DEPRECATION_ERROR")
+            (internalScriptingRunSuspend {
+        resolver.resolveFromScriptSourceAnnotations(annotations.filter { it.annotation is DependsOn || it.annotation is Repository })
+    })
         } catch (e: Throwable) {
-            ResultWithDiagnostics.Failure(*diagnostics.toTypedArray(), e.asDiagnostics(path = context.script.locationId))
+            diagnostics.add(e.asDiagnostics(path = context.script.locationId))
+            ResultWithDiagnostics.Failure(diagnostics)
         }
 
         return resolveResult.onSuccess { resolvedClassPath ->
             ScriptCompilationConfiguration(context.compilationConfiguration) {
                 updateClasspath(resolvedClassPath)
-                if (importedSources.isNotEmpty()) importScripts.append(importedSources)
+                if (importedSources.isNotEmpty()) importScripts.append(importedSources.values.map { FileScriptSource(it.first) })
                 if (compileOptions.isNotEmpty()) compilerOptions.append(compileOptions)
             }.asSuccess()
         }
