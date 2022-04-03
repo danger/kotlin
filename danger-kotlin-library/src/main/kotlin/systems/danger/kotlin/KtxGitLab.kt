59311953d0122091d42ff5e4b3324b4c7ce63686
Andrey Tolmachev <a.tolmachev@redmadrobot.com>
2022-04-03 23:17:45 +0300

    GitLab: Added projectUrl toWebUrl toWebDiffUrl extensions

--- danger-kotlin-library/src/main/kotlin/systems/danger/kotlin/KtxGitLab.kt
+++ danger-kotlin-library/src/main/kotlin/systems/danger/kotlin/KtxGitLab.kt
@@ -1,3 +1,19 @@
 package systems.danger.kotlin
 
+import systems.danger.kotlin.models.gitlab.GitLab
+import java.security.MessageDigest
+
 // extensions over [GitLab] object
+
+val GitLab.projectUrl: String
+    get() = mergeRequest.webUrl.split("/merge_requests").first()
+
+fun GitLab.toWebUrl(filePath: String) = "$projectUrl/blob/${mergeRequest.sha}/${filePath}"
+
+fun GitLab.toWebDiffUrl(filePath: String) = "${mergeRequest.webUrl}/diffs#diff-content-${filePath.sha1}"
+
+val String.sha1: String
+    get() = MessageDigest
+        .getInstance("SHA-1")
+        .digest(toByteArray())
+        .joinToString(separator = "") { "%02x".format(it) }
